26a7ea4117570471d58a69c4b7a68c0650f99a4d
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-02-28 09:10:50 +0300

    chore: add absolute import config

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -1,18 +1,13 @@
 import { ThemeProvider } from "@mui/material";
 import React, { PropsWithChildren, useState } from "react";
-
 import { Provider } from "react-redux";
 
-export const NavigationStateOnMobileContext = React.createContext({
-  show: true,
-  setShow: () => {},
-});
+import { theme } from "@/data/theme";
+import { store } from "@/redux/store";
 
 interface IAppProviderProps {}
 
 const AppProvider = ({ children }: PropsWithChildren<IAppProviderProps>) => {
-  const [showNavigationOnMobile, setShowNavigationOnMobile] = useState(true);
-
   return (
     <div style={{ margin: 0 }}>
       <Provider store={store}>
