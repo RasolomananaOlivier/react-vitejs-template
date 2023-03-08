be1e144d0ff2d2df99f0c4490173e694c2bba7ae
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-08 13:05:51 +0300

    refactor: add services folder

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,18 +2,16 @@ import { ThemeProvider } from "@mui/material";
 import React, { PropsWithChildren, useState } from "react";
 import { Provider } from "react-redux";
 
-import { theme } from "@/data/theme";
+import { theme } from "@/constants/theme";
 import { store } from "@/redux/store";
 
 interface AppProvidersProps extends PropsWithChildren {}
 
 const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
   return (
-    <div style={{ margin: 0 }}>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
   );
 };
 
